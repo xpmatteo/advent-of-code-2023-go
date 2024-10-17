f916f36feedc2858d886a2c7685639226e0c61dd
Matteo Vaccari <vaccari@pobox.com>
2024-10-17 15:19:07 +0200

    day15: parsing commands

--- advent-day15/advent15_test.go
+++ advent-day15/advent15_test.go
@@ -1,8 +1,10 @@
 package advent_day15
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"os"
+	"strconv"
 	"strings"
 	"testing"
 )
@@ -19,12 +21,84 @@ func Test_encodingString(t *testing.T) {
 		{"ignores newlines", "H,\nHASH", 252},
 		{"sample", "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7", 1320},
 		{"part1", readFile("input.txt"), 495972},
+		{"rn", "rn", 0},
+		{"cm", "cm", 0},
+		{"qp", "qp", 1},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			assert.Equal(t, test.want, hash(test.input))
 		})
 	}
+}
+
+func Test_parseCommands(t *testing.T) {
+	tests := []struct {
+		input string
+		want  *Command
+	}{
+		{"rn-", &Command{"rn", 0, remove, 0}},
+		{"qp-", &Command{"qp", 1, remove, 0}},
+		{"qp=3", &Command{"qp", 1, add, 3}},
+	}
+	for _, test := range tests {
+		t.Run(test.input, func(t *testing.T) {
+			assert.Equal(t, test.want, parseCommand(test.input))
+		})
+	}
+}
+
+func parseCommand(input string) *Command {
+	if input[len(input)-1] == '-' {
+		label := strings.Split(input, "-")[0]
+		return &Command{
+			label: label,
+			index: hash1(label),
+			kind:  remove,
+			focal: 0,
+		}
+	} else if strings.Contains(input, "=") {
+		split := strings.Split(input, "=")
+		label, focalString := split[0], split[1]
+		focal, err := strconv.Atoi(focalString)
+		if err != nil {
+			panic(err)
+		}
+		return &Command{
+			label: label,
+			index: hash1(label),
+			kind:  add,
+			focal: focal,
+		}
+	} else {
+		panic("bad input command: " + input)
+	}
+}
+
+type CommandKind int
+
+var add CommandKind = 1
+var remove CommandKind = 2
+
+func (k CommandKind) String() string {
+	if k == add {
+		return "add"
+	} else if k == remove {
+		return "remove"
+	} else {
+		panic(fmt.Errorf("invalid CommandKind %d", k))
+	}
+}
+
+type Command struct {
+	label string
+	index int
+	kind  CommandKind
+	focal int
+}
+
+func (c *Command) String() string {
+	return fmt.Sprintf("%s %d %s %d", c.label, c.index, c.kind, c.focal)
 }
 
 func readFile(fileName string) string {
